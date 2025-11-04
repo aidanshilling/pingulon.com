94ac7de502a9b8a096e8a7cfe9cb3b36a92e21f7
aidanshilling <aidanshilling91@gmail.com>
2025-11-04 00:54:04 +0000

    Add test fetch of content from server

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageProps } from "./$types";
+	import { marked } from "marked";
+
+	let { data }: PageProps = $props();
+</script>
+
+<div>{@html marked.parse(data.story.body)}</div>
---
+++ src/routes/+page.ts
@@ -0,0 +1,11 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ params }) => {
+	const res = await fetch('http://localhost:1323/stories/Testy');
+	if (!res.ok) throw new Error(`HTTP ${res.status}`);
+	const story = await res.json()
+	return { story };
+
+	error(404, 'Not found');
+};
